b9a3decbd7a5e4576f80d1c708aca473560950c0
Diego Vazquez Nanini <diego.vazqueznanini@deliveroo.co.uk>
2021-03-04 09:13:27 +0000

    PRDSC-61: Add SNYK_SEVERITY_THRESHOLD (#56)
    PRDSC-61: Add SNYK_SEVERITY_THRESHOLD

--- scripts/container_scan.sh
+++ scripts/container_scan.sh
@@ -10,12 +10,17 @@ if [ -z "${GITHUB_SNYK_TOKEN}" ]; then
   exit 1
 fi
 
-if [[ $CONTAINER_TAG ]]; then
-  TAG_NAME=$CONTAINER_TAG
-  echo "Tag from \$CONTAINER_TAG: $CONTAINER_TAG"
-else
-  TAG_NAME="latest"
-fi
+TAG_NAME=${CONTAINER_TAG:="latest"}
+SEVERITY_THRESHOLD=${SNYK_SEVERITY_THRESHOLD:="high"}
+
+parse_and_post_comment () {
+  scan_results=$(parse_scan_results $1)
+  if [[ $scan_results ]]; then
+      comment_on_pr "$scan_results"
+  else
+    echo "No scan results found in $1."
+  fi
+}
 
 ## auth
 snyk auth ${SNYK_TOKEN}
@@ -30,7 +35,7 @@ SNYK_FNAME=snyk.json
 docker image tag ${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1} ${CIRCLE_PROJECT_REPONAME}:${TAG_NAME}
 
 ## test 
-snyk test --docker ${CIRCLE_PROJECT_REPONAME}:${TAG_NAME} --file=${PROJECT_PATH}/Dockerfile --json > ${PROJECT_PATH}/${SNYK_FNAME}
+snyk test --severity-threshold=${SEVERITY_THRESHOLD} --docker ${CIRCLE_PROJECT_REPONAME}:${TAG_NAME} --file=${PROJECT_PATH}/Dockerfile --json > "${PROJECT_PATH}/${SNYK_FNAME}"
 
 echo "[*] Finished snyk test. Moving onto monitor"
 
@@ -40,12 +45,8 @@ snyk monitor --docker ${CIRCLE_PROJECT_REPONAME}:${TAG_NAME} --file="${PROJECT_P
 echo "[*] Finished snyk monitoring. Checking if we need to send results to GitHub"
 
 ## parse results and check if we should comment back to GitHub
-scan_results=$(parse_scan_results ${PROJECT_PATH}/${SNYK_FNAME})
-
 if [[ -z "${CIRCLE_PULL_REQUEST}" ]]; then
   echo "Not a pull request. Exiting"
-elif [[ $scan_results ]]; then
-   	comment_on_pr "$scan_results"
 else
-	echo "No scan results found. Exiting."
+  parse_and_post_comment "${PROJECT_PATH}/${SNYK_FNAME}"
 fi
